b14eeb0e2a882d4e2ed3a383b5697733a1b0b501
davekessener <davekessener@gmail.com>
2015-10-26 13:43:42 +0100

    changed start

--- TI3_AD/src/Start.java
+++ TI3_AD/src/Start.java
@@ -1,3 +1,6 @@
+import lab1.test.ListDigest;
+import lab2.test.PrimeSearchDigest;
+import lab4.test.PascalDigest;
 import lab5.test.SortDigest;
 
 
@@ -5,6 +8,9 @@ public class Start
 {
     public static void main(String[] args)
     {
+        (new ListDigest()).run();
+        (new PrimeSearchDigest()).run();
+        (new PascalDigest()).run();
         (new SortDigest()).run();
     }
 }
